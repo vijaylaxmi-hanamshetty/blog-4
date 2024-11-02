ebe2883f69f20666a0281ea14dd83907f8e2e7cd
vijaylaxmi hanamshetty <vijaylaxmihanamshetty38@.in>
2024-11-02 18:41:05 +0530

    add models accprding to page

---
+++ models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String,ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    role = Column(String)  
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User")
